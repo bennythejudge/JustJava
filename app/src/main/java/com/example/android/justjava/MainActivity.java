7e0aa3f050eb1a8f13ac7f317291e59457ab3931
Benedetto Lo Giudice <logube@gmail.com>
2017-11-14 23:42:09 +0100

    Complete course

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -32,8 +32,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(quantity);
-        displayPrice(quantity * unit_price);
+        String priceMessage = "That would be $" + (quantity * unit_price) + " for " + quantity +
+                " coffees, please.";
+        // display(quantity);
+        displayMessage(priceMessage);
     }
 
     public void increment(View view) {
@@ -56,6 +58,11 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
     /**
